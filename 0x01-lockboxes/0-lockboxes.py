cc74011199d4cd8cfa7ddc91e47d9e0ba6bebb79
Usman Akinyemi <usmanakinyemi202@gmail.com>
2024-07-02 15:21:59 -0400

    Completed the task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+!/usr/bin/python3
+"""Solve the lockbox problem"""
+
+
+def canUnlockAll(boxes):
+    """Solve the lockbox problem"""
+    res = {"0": True}
+    for box in boxes:
+        if not box:
+            break
+        for key in box:
+            if key in res:
+                continue
+            res[f"{key}"] = True
+    if len(boxes) == len(res):
+        return True
+    return False
